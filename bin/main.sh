6fbffcee0f70fff0adc8786cbd1c3b6eeeaa17f3
duanzengguang <duanzengguang@duanzengguangde.local>
2018-11-09 11:34:20 +0800

    mas install

--- bin/main.sh
+++ bin/main.sh
@@ -6,15 +6,30 @@ CURRENT_HOME=~/myRepo/myconfigs/bin
 if [[ ! -f '/usr/local/bin/brew' ]];then
 	/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 	brew install vim
-	# git 扩展
-	brew install git-extras
+	brew install git-extras # git 扩展
 	brew install imagemagic
 	brew install macdown
 	brew install mas
 	brew install google-chrome
+	brew install iterm2
+
+	# mas	
+	mas install 497799835 # Xcode
+#	mas install 405843582 # Alfred  版本太老
+	mas install 1356055347 # 印象笔记
+	mas install 425424353 # The Unarchiver
+#	mas install 1181350496 # WebFont 
+#	mas install 789066512 # Maipo
+	mas install 944848654 # 网易云音乐
+	mas install 451108668 # QQ 
+	mas install 836500024 # WeChat
+	mas install 488709442 # Visual JSON
+	mas install 491854842 # 有道翻译
+	mas install 702682966 # 搜狗输入助手
 	
+	# CocoaPods
+	sudu gem install cocoapods
 	
-	mas install 
 fi 
 
 # alias
