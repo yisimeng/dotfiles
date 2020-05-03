4b5541f01ba81cb79b115e15e81e8e6598d2b963
忆思梦 <465386131@qq.com>
2020-05-03 18:28:34 +0800

    _find file

--- bin/_find.sh
+++ bin/_find.sh
@@ -1,12 +1,8 @@
 #!/bin/sh
 
 _find(){
-	find $2 -name "*.$1" | while read i
+    find $2 -name "$1" | while read i
 do
 	echo $i
 done
 }
-find $2 -name "*.$1" | while read i
-do
-echo $i  #显示文件名称
-done
