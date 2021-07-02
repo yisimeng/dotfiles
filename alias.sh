13964525ad0a7ec0b78bacbd07c8b78f97b9a998
duanzengguang <duanzengguang@meituan.com>
2021-07-02 16:47:40 +0800

    alias

--- alias.sh
+++ alias.sh
@@ -8,6 +8,10 @@ alias ll="ls -l"
 
 alias rmdir="rm -fr"
 
+# 快捷打开工程
+alias vso="code ."
+alias xco='open *.xcworkspace || open *.xcodeproj'
+
 # hosts
 alias vihosts="sudo vi /etc/hosts"
 alias cathosts="cat /etc/hosts"
